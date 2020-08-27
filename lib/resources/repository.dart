ad7cf938159c39722c8f67fa04474ccbf17e8ffb
anhvu-95 <vu.haianh291@gmail.com>
2020-08-27 13:09:00 +0300

    Feat: Add movie bloc. Refactor UI

--- lib/resources/repository.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-import 'movie_api.dart';
-import '../models/item.dart';
-
-class Repository {
-  final moviesApiProvider = MovieApiProvider();
-
-  Future<ItemModel> fetchAllMovies() => moviesApiProvider.fetchMovieList();
-}
