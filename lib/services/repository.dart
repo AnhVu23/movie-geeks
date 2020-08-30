e7fb2dc86c11fb76a2d28dce6b2622154a936081
anhvu-95 <vu.haianh291@gmail.com>
2020-08-30 10:58:06 +0300

    Feat: Add blocs to handle movie detail

--- lib/services/repository.dart
+++ lib/services/repository.dart
@@ -1,9 +1,14 @@
 import 'dart:async';
+import 'package:movie_geeks/models/trailer.dart';
+
 import 'movie_api.dart';
 import '../models/item.dart';
+import '../models/trailer.dart';
 
 class Repository {
   final moviesApiProvider = MovieApiProvider();
 
   Future<ItemModel> fetchAllMovies() => moviesApiProvider.fetchMovieList();
+  Future<TrailerModel> fetchTrailers(int movieId) =>
+      moviesApiProvider.fetchTrailer(movieId);
 }
